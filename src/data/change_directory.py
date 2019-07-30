ea1dd0e1990602415c2b1112a9276ca15ea5da2f
Ngoc <ntran19@jhu.edu>
2019-07-30 14:52:12 -0400

    Fixed data path

--- src/data/change_directory.py
+++ src/data/change_directory.py
@@ -17,7 +17,7 @@ class ChangeDirectory:
         
     def change_to_data_dir(self):
         parent_path = Path(self.MAIN_DIR).parent
-        data_path = str(parent_path) + "/data/raw"
+        data_path = str(parent_path) + "/data"
         os.chdir(data_path)
         
     def change_to_notebook_dir(self):
